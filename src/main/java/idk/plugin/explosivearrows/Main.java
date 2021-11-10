6f8a982d39b656ed406607559b90b7e48f2946bf
Petteri <26197131+PetteriM1@users.noreply.github.com>
2021-11-10 10:13:25 +0200

    Option to not break blocks

--- src/main/java/idk/plugin/explosivearrows/Main.java
+++ src/main/java/idk/plugin/explosivearrows/Main.java
@@ -3,6 +3,7 @@ package idk.plugin.explosivearrows;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.projectile.EntityArrow;
 import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.entity.ProjectileHitEvent;
 import cn.nukkit.level.Explosion;
@@ -10,17 +11,21 @@ import cn.nukkit.plugin.PluginBase;
 
 public class Main extends PluginBase implements Listener {
 
+    @Override
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(this, this);
         this.saveDefaultConfig();
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onProjectileHit(ProjectileHitEvent e) {
         Entity projectile = e.getEntity();
-        if (!(projectile instanceof EntityArrow)) return;
-        Explosion explosion = new Explosion(projectile, this.getConfig().getDouble("explosionSize", 2.0), projectile);
-        explosion.explodeA();
-        explosion.explodeB();
+        if (projectile instanceof EntityArrow) {
+            Explosion explosion = new Explosion(projectile, this.getConfig().getDouble("explosionSize", 2.0), projectile);
+            if (this.getConfig().getBoolean("breakBlocks", true)) {
+                explosion.explodeA();
+            }
+            explosion.explodeB();
+        }
     }
 }
